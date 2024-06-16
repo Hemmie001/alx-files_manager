5028fb50ede583402f46b5a1944d2ae246d97281
Hemmie001 <emmanuelakej@gmail.com>
2024-06-16 12:46:26 -0700

    3

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@
 const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 // GET /status => AppController.getStatus
 router.get('/status', AppController.getStatus);
 
 // GET /stats => AppController.getStats
 router.get('/stats', AppController.getStats);
+
+// POST /users => UsersController.postNew
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
