fa98abcbaf87c34b88a0e5d17619f65f0eac7997
Hemmie001 <emmanuelakej@gmail.com>
2024-06-16 16:14:52 -0700

    5

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
 // GET /status => AppController.getStatus
 router.get('/status', AppController.getStatus);
@@ -23,5 +24,9 @@ router.get('/disconnect', AuthController.getDisconnect);
 
 // GET /users/me => UsersController.getMe
 router.get('/users/me', UsersController.getMe);
+
+// POST /files => FilesController.postUpload
+router.post('/files', FilesController.postUpload);
+
 
 module.exports = router;
