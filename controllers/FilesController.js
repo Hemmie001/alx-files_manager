2f241489018d88c3a92950c62bbd74f537e05882
Hemmie001 <emmanuelakej@gmail.com>
2024-12-13 08:28:34 -0800

    check for token in both header3

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -9,7 +9,8 @@ const redisClient = require('../utils/redis');
 
 class FilesController {
   static async postUpload(req, res) {
-    const token = req.headers.authorization;
+    // Check for the token in both headers
+    const token = req.headers.authorization || req.headers['x-token'];
 
     if (!token) {
       return res.status(401).json({ error: 'Unauthorized' });
